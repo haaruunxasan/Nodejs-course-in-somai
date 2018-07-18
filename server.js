951af0de53e3724b113153ed3308546a3db66f0e
Somali-Developer <38513898+Somali-Developer@users.noreply.github.com>
2018-07-18 10:18:47 +0300

    adding handle bars and saving to json data

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const Employee = require('./employee');
 const express = require('express');
 // waxan kusoo darsaneenaa body-parser
 const bodyParser = require('body-parser')
+//adding hbs template engine
+const hbs = require('hbs');
 const app = express();
 
+// body-parser midleware
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+// setting hbs midleware
+app.set('view engine', 'hbs')
+
+// registring partials
+hbs.registerPartials( __dirname + '/views/partials')
+// hbs.registerPartial('views/partials')
 
 app.use(express.static(__dirname + '/public'))
 
@@ -13,16 +23,24 @@ app.get('/',(req, res)=>{
     // console.log('we get a new client')
     // res.send(Employee.addEmployee('Ali farah yalaxow','cleaner','male'))
 
-    res.sendfile('./public/hom.html')
+    // res.sendfile('./public/hom.html')
+    res.sendFile( __dirname+'/public/hom.html')
 })
 
 app.post('/',(req,res)=>{
-    console.log(req.body.Title)
+    // console.log(req.body)
+    // saving emplyee
+    Employee.addEmployee(req.body.fName,req.body.Title,req.body.Gender)
+    // saving list of employee as an array 
+    var AllEmpList = Employee.readAllEmployee();
+
+    //reading All employees after post or saving a new employee
+    res.render('home',{AllEmpList})
 })
 
-app.get('/nagusabsan',(req,res)=>{
-    console.log('URL: '+req.url + ' ' + req.method)
-    res.sendfile('./public/about.html')
+app.get('/hbs',(req,res)=>{
+   var AllEmpList = Employee.readAllEmployee();
+    res.render('home',{AllEmpList})
 })
 
 app.listen(3000,()=>{
